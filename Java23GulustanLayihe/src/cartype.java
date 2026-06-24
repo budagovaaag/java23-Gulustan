44aa189299ac59f1ea6587495511194b37a32cbf
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-25 00:22:35 +0400

    Create cartype.java

---
+++ Java23GulustanLayihe/src/cartype.java
@@ -0,0 +1,10 @@
+
+public class cartype {
+	public static void main(String[] args) {
+		System.out.println("bmw");
+		System.out.println("mercedes");
+		System.out.println("kia");
+		System.out.println("toyota");
+	}
+
+}
