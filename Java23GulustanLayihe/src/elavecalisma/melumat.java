fc48f33acc7e104bdf1c42bce22707c5fcf6a372
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 00:03:24 +0400

    Create melumat.java

---
+++ Java23GulustanLayihe/src/elavecalisma/melumat.java
@@ -0,0 +1,15 @@
+package elavecalisma;
+
+public class melumat {
+	public static void main(String[] args) {
+		String name="Gülüstan";
+        int age=18;
+        double height=1.64;
+        boolean beStudent=true;
+    
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(beStudent);
+	}	         
+}
