33e8d7e4bed14de48703a0a526ce302590bdec3d
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 00:06:24 +0400

    Create Deyisenler.java

---
+++ Java23GulustanLayihe/src/elavecalisma/Deyisenler.java
@@ -0,0 +1,11 @@
+package elavecalisma;
+
+public class Deyisenler {
+	public static void main(String[] args) {
+        char herf='G';
+        herf++;
+    
+        System.out.println(herf);
+        
+   } 
+}
