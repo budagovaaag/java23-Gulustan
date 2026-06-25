54196bd285906ee16d26ce9867bbcde1b968a249
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 00:05:06 +0400

    Create Suffix.java

---
+++ Java23GulustanLayihe/src/elavecalisma/Suffix.java
@@ -0,0 +1,11 @@
+package elavecalisma;
+
+public class Suffix {
+    public static void main(String[] args) {
+        long n=8000000000L;
+        float k=12.99F;
+        System.out.println(n);
+        System.out.println(k);
+        
+   } 
+}
