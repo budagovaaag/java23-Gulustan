4477c91c839c9a49fe86556006fa17b76edee24a
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-25 22:58:39 +0400

    Create evisi2.java

---
+++ Java23GulustanLayihe/src/der1calismalar/evisi2.java
@@ -0,0 +1,18 @@
+package der1calismalar;
+
+public class evisi2 {
+	public static void main(String[] args) {
+		int age=25;
+		boolean hasJob= false;
+		char studentGrade='C';
+		double carPrice=25000;
+		String employeeName="Melik";
+		
+		System.out.println(age);
+		System.out.println(hasJob);
+		System.out.println(studentGrade);
+		System.out.println(carPrice);
+		System.out.println(employeeName);
+
+	}
+}
