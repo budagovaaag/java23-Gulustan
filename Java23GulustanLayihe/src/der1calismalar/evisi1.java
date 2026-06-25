c5eaf2dcf352879006ce75853b4cbe02811365ba
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-25 22:51:04 +0400

    evisi1

---
+++ Java23GulustanLayihe/src/der1calismalar/evisi1.java
@@ -0,0 +1,24 @@
+package der1calismalar;
+
+public class evisi1 {
+	public static void main(String[] args)  {
+		int age=7;
+		age=14;
+		byte b=7;
+		short s=7;
+		long l=7;
+		double d=7.9D;
+		float f=7.9F;
+		char c='A';
+		boolean hasHome=true;
+		
+		System.out.println(age);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(hasHome);
+	}
+}
