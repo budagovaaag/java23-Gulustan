bea91c2681cef419435f6fe35fc09cf57d8e47e5
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 00:10:24 +0400

    Create Melumatt.java

---
+++ Java23GulustanLayihe/src/elavecalisma/Melumatt.java
@@ -0,0 +1,12 @@
+package elavecalisma;
+
+public class Melumatt { 
+	public static void main(String[] args) {
+        int a=27;
+        int b=4;
+        int c=a%b;
+    
+        System.out.print("Netice : "+c);
+        
+   } 
+}
