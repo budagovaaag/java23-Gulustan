a01a5e07f40456e217e644aabf94c1eb1ecc2fd0
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-25 22:39:51 +0400

    Create VariablesPractice.java

---
+++ Java23GulustanLayihe/src/src/VariablesPractice.java
@@ -0,0 +1,12 @@
+package src;
+
+public class VariablesPractice  {
+	public static void main(String[] args) {
+		int age=20;
+		boolean hasHome= true;
+		char studentGrade='A';
+		double productPrice=3.8;
+		String studentName="Yaqub";
+
+	}
+}
