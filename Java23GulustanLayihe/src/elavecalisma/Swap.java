ff2e3066d74ca8a0bfabab8b74e44fa4264ea2d0
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 00:11:44 +0400

    Create Swap.java

---
+++ Java23GulustanLayihe/src/elavecalisma/Swap.java
@@ -0,0 +1,15 @@
+package elavecalisma;
+
+public class Swap {
+	public static void main(String[] args) {
+        int x=50;
+        int y=100;
+        
+        int c=x;
+        x=y;
+        y=c;
+
+     System.out.println(x);
+     System.out.println(y);
+ }
+}
