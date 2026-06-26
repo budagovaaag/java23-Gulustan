dcbbd84a214cc41d66d91b2d73211febb35f1c57
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 13:10:14 +0400

    Create Unory.java

---
+++ Java23GulustanLayihe/src/ders2/Unory.java
@@ -0,0 +1,13 @@
+package ders2;
+
+public class Unory {
+	public static void main(String[] args) {
+		byte a=6;
+		System.out.println(a++);
+		System.out.println(++a);
+		short b=22;
+		System.out.println(b--);
+		b--;
+		System.out.println(b);
+	}
+}
