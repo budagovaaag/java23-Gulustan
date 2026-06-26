5927157737be7d2a342e1674e28dc898f869ed23
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 12:58:25 +0400

    Create Assignment.java

---
+++ Java23GulustanLayihe/src/ders2/Assignment.java
@@ -0,0 +1,21 @@
+package ders2;
+
+public class Assignment {
+	public static void main(String[] args) {
+		int a=22;
+		a+=4;
+		int b=a;
+		b-=6;
+		int c=18;
+		c*=2;
+		int d=b;
+		d/=4;
+		int e=27;
+		e%=4;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+}
