f4540adde5167bb77824a1aa696e087a53b12af2
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-26 13:04:07 +0400

    Create Arithmetic.java

---
+++ Java23GulustanLayihe/src/ders2/Arithmetic.java
@@ -0,0 +1,13 @@
+package ders2;
+
+public class Arithmetic {
+	public static void main(String[] args) {
+		int f=18;
+		System.out.println(f);
+		f++;
+		System.out.println(f);
+		f--;
+		f--;
+		System.out.println(f);
+	}
+}
