054bc93d755068e33b6de605296c73bceed0972a
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-27 21:08:56 +0400

    Create IfElse.java

---
+++ Java23GulustanLayihe/src/ders5/IfElse.java
@@ -0,0 +1,17 @@
+package ders5;
+
+public class IfElse {
+	public static void main(String[] args) {
+		
+		String password="2206";
+		String daxiledilmishcode="xxxx";
+		
+		if( daxiledilmishcode==password) {
+			System.out.println("shifre ugurludur");
+			
+		}else {
+			System.out.println("shifre yanlishdir");
+		}
+	}
+
+}
