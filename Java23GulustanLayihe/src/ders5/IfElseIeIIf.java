0b729acd013ce8631c18f34b654e47135dde4c86
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-27 21:23:10 +0400

    Create IfElseIeIIf.java

---
+++ Java23GulustanLayihe/src/ders5/IfElseIeIIf.java
@@ -0,0 +1,16 @@
+package ders5;
+
+public class IfElseIeIIf {
+	public static void main(String[] args) {
+		int n1=22;
+		int n2=6;
+		char op='%';
+		if (op=='+') {
+		System.out.println(n1 + "+" + n2);
+		}else if(op=='-') {
+			System.out.println(n1+ "-" + n2);
+		}else {
+			System.out.println(n1+ "%" + n2);
+		}
+	}
+}
