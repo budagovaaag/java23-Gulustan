72b9afcad9f20607e5167481cd2e7e99cba721cf
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-27 21:39:20 +0400

    Create CutTek.java

---
+++ Java23GulustanLayihe/src/CutTek.java
@@ -0,0 +1,13 @@
+
+public class CutTek {
+	public static void main(String[] args) {
+		int n=2008;
+		
+		if(n%2==0) {
+			System.out.println("cutdur");
+		}else {
+			System.out.println("tekdir");
+		}
+	}
+
+}
