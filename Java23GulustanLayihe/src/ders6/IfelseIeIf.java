ecc518a4fd20dc890edcad5a4934e412324659d2
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-27 21:56:34 +0400

    Create IfelseIeIf.java

---
+++ Java23GulustanLayihe/src/ders6/IfelseIeIf.java
@@ -0,0 +1,22 @@
+package ders6;
+
+public class IfelseIeIf {
+	public static void main(String[] args) {
+		int bal=93;
+		
+		if (bal>90) {
+			System.out.println("A");
+		}else if(bal>80) {
+			System.out.println("B");
+		}else if(bal>70) {
+			System.out.println("C");
+		}else if(bal>60) {
+			System.out.println("D");
+		}else if(bal>50) {
+			System.out.println("E");
+		}else {
+			System.out.println("KESILDIN");
+		}
+	}
+
+}
