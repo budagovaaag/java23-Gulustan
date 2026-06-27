343b913c2fd86bae12c1c2824c3817ae1a6a55f7
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-27 21:56:15 +0400

    Create VurmaCedveli.java

---
+++ Java23GulustanLayihe/src/ders6/VurmaCedveli.java
@@ -0,0 +1,13 @@
+package ders6;
+
+public class VurmaCedveli {
+	public static void main(String[] args) {
+		int i=7;
+		
+		for (int a=1; a<=10; a++ ) {
+			System.out.println(i + "*" + a + "=" + (i*a)  );
+
+		}
+	}
+
+}
