38da7c314ba2169a25fb258255ecdc8711115367
budagovaaag <gulustanbudaqova01@gmail.com>
2026-06-28 22:26:08 +0400

    Create dersprak1.java

---
+++ Java23GulustanLayihe/src/ders7/dersprak1.java
@@ -0,0 +1,37 @@
+package ders7;
+
+public class dersprak1 {
+	// shortdan byte
+	public static void main(String[] args) {
+		short s=69;
+		byte k= (byte) s;
+		System.out.println(k);
+		
+	//short dan byte2
+		short m=589;
+		byte f=(byte) m;
+		System.out.println(f);
+		
+	//long dan int e
+		long l=458L;
+		int e=(int) l;
+		System.out.println(e);
+		
+	//double den floata
+		double a=5632.6;
+		float y= (float) a;
+		System.out.println(y);
+		
+	//char dan int e
+		char c='D';
+		int x= c;
+		System.out.println(x);
+		
+	//int den char a
+		int q=123;
+		char w=(char) q;
+		System.out.println(w);
+		
+	}
+
+}
