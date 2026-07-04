dd47ba9fb56ddbca8f6d45fc4951246ffaa72424
budagovaaag <gulustanbudaqova01@gmail.com>
2026-07-04 22:52:13 +0400

    Create Calculator.java

---
+++ Java23GulustanLayihe/src/ders9/Calculator.java
@@ -0,0 +1,13 @@
+package ders9;
+
+public class Calculator {
+	public double average(int a, int b) {
+		return (a+b)/2.0;
+	}
+
+	public static void main(String[] args) {
+		Calculator obj=new Calculator();
+		System.out.println(obj.average(22, 6));
+		System.out.println(obj.average(18, 22));
+	}
+}
