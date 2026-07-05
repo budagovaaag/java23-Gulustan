d8c04ea721b98bcff872e4607962c18b0f340a8a
budagovaaag <gulustanbudaqova01@gmail.com>
2026-07-05 16:17:39 +0400

    Create DiscountCalculator.java

---
+++ Java23GulustanLayihe/src/ElaveTestler/DiscountCalculator.java
@@ -0,0 +1,11 @@
+package ElaveTestler;
+
+public class DiscountCalculator {
+	public void calculateDiscount(Computer computer) {
+		if(computer.ram==8) {
+			computer.price=computer.price-100.0;
+		}
+		
+	}
+
+}
