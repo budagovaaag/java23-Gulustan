c9ec9e29da836dd68a1cc712ba55f4bec782a23a
budagovaaag <gulustanbudaqova01@gmail.com>
2026-07-05 15:32:45 +0400

    Create Person.java

---
+++ Java23GulustanLayihe/src/ders9/Person.java
@@ -0,0 +1,20 @@
+package ders9;
+
+public class Person {
+	String name;
+	String surname;
+	int experienceYear;
+	double salary;
+	String phone;
+	
+	
+	public void printInfo(){
+		System.out.println("person info");
+		System.out.println(name);
+		System.out.println(surname);
+		System.out.println(experienceYear);
+		System.out.println(salary);
+		System.out.println(phone );
+		
+	}
+}
