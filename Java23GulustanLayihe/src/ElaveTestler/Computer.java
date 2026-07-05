599d47412130d26abd3a20b9aed40e3a8fefd00e
budagovaaag <gulustanbudaqova01@gmail.com>
2026-07-05 16:16:54 +0400

    Create Computer.java

---
+++ Java23GulustanLayihe/src/ElaveTestler/Computer.java
@@ -0,0 +1,15 @@
+package ElaveTestler;
+
+public class Computer {
+	String brand;
+	int ram;
+	double price;
+	
+	public void showDetails() {
+		System.out.println("Komp info");
+		System.out.println(brand);
+		System.out.println(ram);
+		System.out.println(price);
+	}
+
+}
